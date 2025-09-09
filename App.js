46ddd29b030f3ba398406bf17acf71efee6a4efa
Khrystyna <khrystyna.koshova@gmail.com>
2025-09-09 13:17:38 +0300

    day1

--- App.js
+++ App.js
@@ -1,10 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
+  const [count, setCount] = useState(0)
+  const [number, setNumber] = React.useState('');
+  const [isResting, setIsResting] = useState(false);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+     let interval = null;
+     if (isRunning) {
+        if (count > 0) {
+            interval = setInterval(() => {
+                setCount(preCount => preCount - 1)
+            }, 1000)
+        } else {
+            if (!isResting) {
+                setCount(5);
+                setIsResting(true);
+            } else {
+                setIsRunning(false);
+                setIsResting(false);
+                setCount(0);
+                }
+            }
+        }
+        return () => clearInterval(interval);
+      }, [isRunning, count, isResting]);
+
+  const startTimer = () => {
+    const newNum = parseInt(number, 10);
+    if (!isNaN(newNum) && newNum > 0) {
+        setCount(25);
+        setNumber(number - 1);
+        setIsRunning(true);
+        setIsResting(false);
+    }
+  };
+
+  const stopTimer = () => {
+      setIsRunning(false);
+      setIsResting(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        onChangeText={setNumber}
+        value={number}
+        placeholder="sessions"
+        keyboardType="numeric"
+       />
+       <Text>{number} Sessions</Text>
+       <Button
+        title="Start Timer"
+        onPress={startTimer}
+        disabled={isRunning}
+       />
+       <Button
+        title="Stop Timer"
+        onPress={stopTimer}
+        disabled={!isRunning}
+        />
+        <Text style={styles.statusText}>
+          {isRunning ? (isResting ? 'Rest' : 'Work') : 'Idle'}
+        </Text>
+       <Text>Timer {count}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +75,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8a9675',
     alignItems: 'center',
     justifyContent: 'center',
   },
